ca4424ce8a5a334ebf4e55996f1c846a84e3db1a
cubarabara <cubarabara@gmail.com>
2023-09-20 09:30:05 +0700

    feature: add new feature zoom in and out, description: There are a number of bugs, I can't go back to the main settings

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ class AcodePlugin {
       // Initialising $page for plugin
       $page.id = 'acode-plugin-package-adder';
       $page.settitle("Responsive");
-      this.$page = $page
+      this.$page = $page;
 
       this.$runBtn = tag('span', {
          className: 'icon play_circle_filled',
@@ -36,22 +36,68 @@ class AcodePlugin {
       // Adding custom styles to $page 
       this.$style = tag('style', {
          textContent: style,
-      })
+      });
 
       this.$divWrapper = tag('div', {
          className: 'wrapper',
-      })
+      });
 
+      // Mobile 1
       this.$iframeMobile = tag('iframe', {
          className: 'iframeMobile',
-         width: "335",
+         width: "355",
          height: "480"
-      })
+      });
+
+      // small tablet 2
+      this.$iframeSmallTablet = tag('iframe', {
+         className: 'iframeMobile',
+         width: "480",
+         height: "640"
+      });
+
+      // medium tablet 3
+      this.$iframeMediumTablet = tag('iframe', {
+         className: 'iframeMobile',
+         width: "665",
+         height: "920"
+      });
+
+      // tablet portrait 4
+      this.$iframeTabletPortrait = tag('iframe', {
+         className: 'iframeMobile',
+         width: "995",
+         height: "1024"
+      });
+
+      // tablet landscape 5
+      this.$iframeTabletLandscape = tag('iframe', {
+         className: 'iframeMobile',
+         width: "1024",
+         height: "768"
+      });
+
+      // desktop 6
+      this.$iframeDesktop = tag('iframe', {
+         className: 'iframeMobile',
+         width: "1200",
+         height: "800"
+      });
 
       document.head.append(this.$style);
-      document.head.querySelector("[name='viewport']").remove();
+      //document.head.querySelector("[name='viewport']").setAttribute("content", "width=1024, height=768");
+      /*document.addEventListener("DOMContentLoaded", function () {
+         document.henquerySelector("[name='viewport']").setAttribute("content", "width=1024, height=768");
+      });*/
+
       this.$page.append(this.$divWrapper)
+
       this.$divWrapper.append(this.$iframeMobile)
+      this.$divWrapper.append(this.$iframeSmallTablet)
+      this.$divWrapper.append(this.$iframeMediumTablet)
+      this.$divWrapper.append(this.$iframeTabletPortrait)
+      this.$divWrapper.append(this.$iframeTabletLandscape)
+      this.$divWrapper.append(this.$iframeDesktop)
 
       this.checkRunnable();
       editorManager.on('switch-file', this.checkRunnable.bind(this));
@@ -66,18 +112,27 @@ class AcodePlugin {
       });
 
       if (result) {
-         this.$iframeMobile.src = result;
          this.$page.show();
+
+         this.$iframeMobile.src = result;
+         this.$iframeSmallTablet.src = result;
+         this.$iframeMediumTablet.src = result;
+         this.$iframeTabletPortrait.src = result;
+         this.$iframeTabletLandscape.src = result;
+         this.$iframeDesktop.src = result;
+         document.head.querySelector("[name='viewport']").setAttribute("content", "width=device-width, initial-scale=1.0");
       }
    }
 
-   async destroy() {
+   async destroy(e) {
       if (this.$runBtn) {
          this.$runBtn.onclick = null;
          this.$runBtn.remove();
       }
       editorManager.off('switch-file', this.checkRunnable.bind(this));
       editorManager.off('rename-file', this.checkRunnable.bind(this));
+      
+      console.log(e)
    }
 
    checkRunnable() {
